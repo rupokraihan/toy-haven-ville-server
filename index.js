df517bf6755d38bd4352bbbdd8165129c2089699
rupokraihan <rupok2321@gmail.com>
2023-05-17 23:42:57 +0600

    install express,mongodb,dotenv,cors

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 5000;
+
+
+
+app.get("/", (req, res) => {
+  res.send("SERVER IS RUNNING");
+});
+
+app.listen(port, () => {
+  console.log(`running on port, ${port}`);
+});
