6d914c8972b7cb490ebd5fa4f6a4175d82c8d2db
rupokraihan <rupok2321@gmail.com>
2023-05-18 00:09:22 +0600

    middleware added

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const app = express();
 const port = process.env.port || 5000;
 
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("SERVER IS RUNNING");
