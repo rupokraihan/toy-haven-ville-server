ff2ec3f51d8ae152f8cc6432a622dd39bc2483da
rupokraihan <rupok2321@gmail.com>
2023-05-21 00:01:39 +0600

    deleted toy implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.port || 5000;
@@ -44,6 +44,14 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+   app.delete("/mytoys/:id", async (req, res) => {
+     const id = req.params.id;
+     const query = { _id: new ObjectId(id) };
+     const result = await productCollection.deleteOne(query);
+     res.send(result);
+   });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
